6ba7df21b95e86ff5e515587fc94815e1993a386
GeorgeBramus <george.bramus@gmail.com>
2017-08-30 16:39:19 +0300

    Script for full db insert in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'nokogiri'
+
+url = 'http://1000mostcommonwords.com/1000-most-common-russian-words/'
+html = open(url)
+
+words = Nokogiri::HTML(html)
+words = words.css('table tbody tr')
+words.each do |word|
+  original_text = word.css('td')[1].text
+  translated_text = word.css('td')[2].text
+  review_date = Date.today + 3.days.since
+
+  Card.create(original_text, translated_text, review_date)
+end

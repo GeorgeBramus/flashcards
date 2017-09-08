0800fa0a33ed8e190be52dcea950720275f29da9
GeorgeBramus <george.bramus@gmail.com>
2017-09-08 13:56:15 +0300

    In the rails app, gems RSpec and Factory_girl were added and a minimal adjustment was made

---
+++ spec/controllers/cards_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe CardsController, :type => :controller do
+  describe "GET #index" do
+    it "responds successfully" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+    it "will show information about one card" do
+      card = Card.all
+      get :index
+      expect(assigns(:card)).to eq([card])
+    end
+  end
+end

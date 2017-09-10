365a1c1788f9254c34851deb67807b604315b1d0
GeorgeBramus <george.bramus@gmail.com>
2017-09-10 17:56:38 +0300

    The server was broken. It is necessary to repair

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -1,15 +1,20 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe CardsController, :type => :controller do
-  describe "GET #index" do
-    it "responds successfully" do
+RSpec.describe CardsController, type: :controller do
+  describe 'GET #index' do
+    it 'responds successfully' do
       get :index
       expect(response.status).to eq(200)
     end
-    it "will show information about one card" do
+    it 'will show information about one card' do
       card = Card.all
       get :index
       expect(assigns(:card)).to eq([card])
+    end
+  end
+  describe '.create' do
+    it 'create card' do
+      FactoryGirl.create(:card)
     end
   end
 end

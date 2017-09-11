392041f8fb7bcc282bcd7550e848833764c1bfa6
GeorgeBramus <george.bramus@gmail.com>
2017-09-11 14:37:08 +0300

    Wrong model testing option

--- spec/factories/card_factory.rb
+++ spec/factories/card_factory.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
   factory :card do
-    original_text { '123' }
-    translated_text { '321' }
+    original_text 'стол'
+    translated_text 'a table'
+  end
+  factory :card_original_text_empty do
+    original_text ''
+    translated_text 'a table'
+  end
+  factory :card_translated_text_empty do
+    original_text 'стол'
+    translated_text ''
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,8 +1,16 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Card, :type => :model do
-  describe "change when creating" do
-    it "field review_date" do
+  describe 'change when creating' do
+    it 'the original_text is empty' do
+      # card = FactoryGirl.build(:card_original_text_empty)
+      expect { FactoryGirl.build(:card_original_text_empty) }.to raise_error
+    end
+    it 'the translated_text is empty' do
+      # card = FactoryGirl.build(:card_translated_text_empty)
+      expect { FactoryGirl.build(:card_translated_text_empty) }.to raise_error
+    end
+    it 'field review_date' do
       card = FactoryGirl.build(:card)
       card.review_date = 3.days.since
     end

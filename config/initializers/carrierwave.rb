cf09a7664f38aad1882a2e2898660d3858119a44
GeorgeBramus <george.bramus@gmail.com>
2017-10-02 17:15:43 +0300

    Downloading images through the CareerShield on a hard disk

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+# require 'carrierwave/storage/fog'
+CarrierWave.configure do |config|
+  config.permissions = 0666
+  config.directory_permissions = 0777
+  config.storage = :file
+  # config.storage = :fog
+  config.ignore_integrity_errors = false
+  config.ignore_processing_errors = false
+  config.ignore_download_errors = false
+end

be555df60f19e47cc974dcb7a6d84d60b34b65fd
GeorgeBramus <george.bramus@gmail.com>
2017-11-04 16:58:57 +0300

    specs for decks v.2

--- spec/features/decks/creation_deck_spec.rb
+++ spec/features/decks/creation_deck_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Deck, type: :feature do
+  let(:user) { FactoryGirl.build(:user) }
+
   it 'Name fields is empty' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    @user = FactoryGirl.create(:user)
+    visit('/users/sign_in')
+    fill_in('Email', with: 'email@email.mail')
+    fill_in('Пароль', with: 'password')
+    click_button('Авторизоваться')
+
     visit('/decks/new')
     fill_in('Name', with: '')
     click_button('Создать')
-    expect(page).to have_content "Original text can't be blank"
+    expect(page).to have_content "Name can't be blank"
   end
 end
